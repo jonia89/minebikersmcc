853b12956c21336b41e420432229a69cd36a7dc7
jonia89 <jonianttila89@hotmail.com>
2024-02-04 13:41:48 +0200

    MP-messut update

--- src/somePosts.js
+++ src/somePosts.js
@@ -1,9 +1,11 @@
 //Uusin postaus aina ensimmäiseksi!
 export const facebookPosts = [
+  "https://www.facebook.com/photo/?fbid=300720049684308&set=a.125268610562787",
   "https://www.facebook.com/photo/?fbid=286179204471726&set=a.125268610562787",
   "https://www.facebook.com/photo/?fbid=205991242490523&set=a.125268610562787",
 ];
 export const instagramPosts = [
+  "https://www.instagram.com/p/C25JraOIWwS/",
   "https://www.instagram.com/p/C1g3jAyoejo/",
   "https://www.instagram.com/p/CwcC7dkIHjy/",
 ];
