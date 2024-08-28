2f0e2b24df95abf19436ec95b88d34a5713b2f87
jonia89 <jonianttila89@hotmail.com>
2024-08-28 13:40:41 +0300

    uusia some-päivityksiä

--- src/somePosts.js
+++ src/somePosts.js
@@ -1,5 +1,6 @@
 //Uusin postaus aina ensimmäiseksi!
 export const facebookPosts = [
+  "https://www.facebook.com/photo/?fbid=414568098299502&set=a.125268610562787",
   "https://www.facebook.com/photo?fbid=391428223946823&set=a.125268610562787",
   "https://www.facebook.com/photo/?fbid=370679592688353&set=pcb.370679646021681",
   "https://www.facebook.com/photo/?fbid=359579443798368&set=pcb.359579687131677",
@@ -8,7 +9,8 @@ export const facebookPosts = [
   "https://www.facebook.com/photo/?fbid=205991242490523&set=a.125268610562787",
 ];
 export const instagramPosts = [
-  'https://www.instagram.com/p/C71yRxkIrvk/',
+  "https://www.instagram.com/p/C71yRxkIrvk/",
+  "https://www.instagram.com/p/C71yRxkIrvk/",
   "https://www.instagram.com/p/C7UKXlgo2la/",
   "https://www.instagram.com/p/C4dja2Ro6Jc/",
   "https://www.instagram.com/p/C25JraOIWwS/",
