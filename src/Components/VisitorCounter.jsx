5d3cb0f1e70256b06920472e9ca6d197e6e9ddeb
jonia89 <jonianttila89@hotmail.com>
2024-08-28 13:39:39 +0300

    kävijälaskuri-komponentti luotu ja lisätty etusivulle

---
+++ src/Components/VisitorCounter.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+import visitorData from "../visitor_data.json";
+import "./VisitorCounter.css";
+
+export default function Visitors() {
+  const [visitors, setVisitors] = useState([]);
+
+  useEffect(() => {
+    setVisitors(visitorData);
+  }, []);
+
+  const sortedVisitors = visitors.sort((a, b) => b.visitors - a.visitors);
+  const topFiveVisitors = sortedVisitors.slice(0, 5);
+
+  return (
+    <div>
+      <div className="visitor-header">
+        <u> Eniten vierailijoita</u>
+      </div>
+      {topFiveVisitors.length > 0 ? (
+        <div className="visitor-data">
+          {topFiveVisitors.map((visitor, index) => (
+            <div key={index}>
+              {visitor.country}: {visitor.visitors}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Loading visitor data...</p>
+      )}
+    </div>
+  );
+}
