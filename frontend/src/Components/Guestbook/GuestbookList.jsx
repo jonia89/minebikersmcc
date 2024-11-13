b49b93d9d9cba68a5656d9603f377824c1620da2
jonia89 <jonianttila89@hotmail.com>
2024-11-13 19:23:28 +0200

    fetces from postgresql db

--- frontend/src/Components/Guestbook/GuestbookList.jsx
+++ frontend/src/Components/Guestbook/GuestbookList.jsx
@@ -4,12 +4,18 @@ export default function GuestbookList(props) {
   return (
     <div>
       {props.messages.length === 0 ? (
-        <div>Ei viestejä</div>
+        <div><h1 style={{color: 'gray', background: 'black'}}>Ei viestejä</h1></div>
       ) : (
         props.messages.map((message) => (
           <div key={message.id}>
-            <div>{message.nickname} {message.date}</div>
-            <div>{message.messageText}</div>
+            <div>
+              {" "}
+              <h2> {message.nimi}</h2>
+            </div>
+            <div>
+              <h3>{message.viesti}</h3>
+              <p style={{color:'gray'}}>Lähetetty: {message.aika}</p>
+            </div>
           </div>
         ))
       )}
--- frontend/src/Pages/Guestbookpage.jsx
+++ frontend/src/Pages/Guestbookpage.jsx
@@ -1,14 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import GuestbookForm from "../Components/Guestbook/GuestbookForm";
 import GuestbookList from "../Components/Guestbook/GuestbookList";
 
 export default function Guestbook() {
-const [messages, setMessages] = useState([{id: 1, nickname: "Hamppi", messageText: "Kovaa ajoa", date: "2024-09-18T14:52:45.000Z"}])
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/vieraskirja");
+        const posts = await response.json();
+        setMessages(posts);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getPosts();
+  }, []);
+console.log(messages)
 
   return (
     <div className="guestbook">
       <GuestbookForm />
-      <GuestbookList messages={messages}/>
+      <GuestbookList messages={messages} />
     </div>
   );
 }
